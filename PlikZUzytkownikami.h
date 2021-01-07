3d5850e14eb47ac8caab581ec81b3f18ba6c71d0
Arkadiusz Rasiński <arkadiusz.rasinski.programista@gmail.com>
2021-01-07 13:42:53 +0100

    Added load user to vector on start program

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 
 #include "Uzytkownik.h"
 #include "MetodyPomocnicze.h"
@@ -14,12 +15,14 @@ class PlikZUzytkownikami {
     string nazwaPlikuZUzytkownikami;
     fstream plikTekstowy;
 
-    bool czyPlikJestPusty();
     string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    bool czyPlikJestPusty();
+    Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 
 public:
     PlikZUzytkownikami();
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    vector <Uzytkownik> wczytajUzytkownikowZPliku();
 };
 
 #endif // PLIKZUZYTKOWNIKAMI_H
